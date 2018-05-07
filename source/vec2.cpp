59ef137692cc0a53e971e2be9f1360699e8dfc47
phillipmudavanhu <philcreativ@gmail.com>
2018-05-07 13:15:08 +0200

    Aufgabe2.2

---
+++ source/vec2.cpp
@@ -0,0 +1,17 @@
+#ifndef VEC2_HPP //This checks if the header is already declared some else.
+#define VEC2_HPP //This then difines the header if not defined already.
+
+class Vec2
+{
+	public:
+		//Constructors
+	    
+	    Vec2(); //default
+	    Vec2(float x, float y); //intialisation with parameters
+
+	    float x;
+		float y;
+
+		};
+
+#endif  // VEC2_HPP
