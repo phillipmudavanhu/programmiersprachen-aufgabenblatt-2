c4b96dfb2b08566dc96cf0a1dd8aab59e15ad4f3
phillipmudavanhu <philcreativ@gmail.com>
2018-05-07 13:33:15 +0200

    Aufgabe2.2b

--- source/tests.cpp
+++ source/tests.cpp
@@ -5,25 +5,25 @@
 
 // Vec2::Vec2(): x(0), y(0)
 TEST_CASE("Testing default Constructor","[Constructor]"){
-	Vec2 a;
-	REQUIRE(a.x == 0.0f);
-	REQUIRE(a.y == 0.0f);
+  Vec2 a;
+  REQUIRE(a.x == 0.0f);
+  REQUIRE(a.y == 0.0f);
 }
 
 
 //Vec2::Vec2(float x, float y): x(x), y(y)
 TEST_CASE("Testing User Defined Constructor","[Constructor]"){
-	Vec2 a{0.0,0.0};
-	REQUIRE(a.x == 0.0f);
-	REQUIRE(a.y == 0.0f);
+  Vec2 a{0.0,0.0};
+  REQUIRE(a.x == 0.0f);
+  REQUIRE(a.y == 0.0f);
 
-	Vec2 b{5.5,2.2};
-	REQUIRE(b.x == 5.5f);
-	REQUIRE(b.y == 2.2f);
+  Vec2 b{5.5,2.2};
+  REQUIRE(b.x == 5.5f);
+  REQUIRE(b.y == 2.2f);
 
-	Vec2 c{-5.5,-2.2};
-	REQUIRE(c.x == -5.5f);
-	REQUIRE(c.y == -2.2f);
+  Vec2 c{-5.5,-2.2};
+  REQUIRE(c.x == -5.5f);
+  REQUIRE(c.y == -2.2f);
 }
 
 
--- source/vec2.cpp
+++ source/vec2.cpp
@@ -1,11 +1,11 @@
 #include "vec2.hpp"
 
 // Default Constructor Method
-Vec2::Vec2() 
-    : x(0), y(0){
+Vec2::Vec2()
+  : x(0), y(0){
 }
 
 // User Defined Constructor Method
 Vec2::Vec2(float x, float y) 
-    : x(x), y(y){
+  : x(x), y(y){
 }
