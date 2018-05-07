db4634e1fc041a66727a1c6e4dc6cc73272ad5f4
phillipmudavanhu <philcreativ@gmail.com>
2018-05-07 13:56:22 +0200

    Add Vec2

--- source/vec2.hpp
+++ source/vec2.hpp
@@ -1,17 +1,21 @@
 #ifndef VEC2_HPP //This checks if the header is already declared some else.
 #define VEC2_HPP //This then difines the header if not defined already.
 
-class Vec2
+struct Vec2
 {
-	public:
-		//Constructors
+  //Constructors since the class is struct all the methods and variables are public by default
 	    
-	    Vec2(); //default
-	    Vec2(float x, float y); //intialisation with parameters
+  Vec2(); //default
+  Vec2(float x, float y); //intialisation with parameters
 
-	    float x;
-		float y;
+  float x;
+  float y;
 
-		};
+  Vec2& operator+=(Vec2 const& v );
+  Vec2& operator-=(Vec2 const& v );
+  Vec2& operator*=(float s);
+  Vec2& operator/=(float s);
+
+};
 
 #endif  // VEC2_HPP
