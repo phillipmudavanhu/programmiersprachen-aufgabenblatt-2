2b69d937a6cb4dd309511d820c78b528fe7fbad7
phillipmudavanhu <philcreativ@gmail.com>
2018-05-07 14:18:24 +0200

    Add implemented the Methods in vec2

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -9,3 +9,31 @@ Vec2::Vec2()
 Vec2::Vec2(float x, float y) 
   : x(x), y(y){
 }
+
+// Addition operator overloading
+Vec2& Vec2::operator+=(Vec2 const& v){
+	this -> x += v.x;
+	this -> y += v.y;
+      return *this; 
+}
+
+// Subtraction operator overloading
+Vec2& Vec2::operator-=(Vec2 const& v){
+	this -> x -= v.x;
+	this -> y -= v.y;
+      return *this; 
+}
+
+// Multiplication operator overloading
+Vec2& Vec2::operator*=(float s){
+	this -> x *= s;
+	this -> y *= s;
+	  return *this; 
+}
+
+// Division operator overloading
+Vec2& Vec2::operator/=(float s){
+   this -> x /= s;
+   this -> y /= s;
+    return *this;
+}
